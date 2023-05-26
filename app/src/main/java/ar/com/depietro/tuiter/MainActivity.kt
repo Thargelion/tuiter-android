dfb99d2c6c2c65a274aa4c1b9d09be71f92e355b
Maximiliano De Pietro <elwindwalker@gmail.com>
2023-05-26 17:26:03 -0300

    feat: :framed_picture: adjust card, add Access Button

--- app/src/main/java/ar/com/depietro/tuiter/MainActivity.kt
+++ app/src/main/java/ar/com/depietro/tuiter/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ar.com.depietro.tuiter.ui.components.LoginUser
 import ar.com.depietro.tuiter.ui.theme.TuiterTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginUser(modifier = Modifier.padding(10.dp))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TuiterTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/ar/com/depietro/tuiter/ui/components/UserForm.kt
+++ app/src/main/java/ar/com/depietro/tuiter/ui/components/UserForm.kt
@@ -1,7 +1,9 @@
 package ar.com.depietro.tuiter.ui.components
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -11,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,18 +21,28 @@ import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginUser() {
+fun LoginUser(modifier: Modifier = Modifier) {
     val paddingModifier = Modifier.padding(10.dp)
     var text by remember { mutableStateOf(TextFieldValue("")) }
     Card(modifier = paddingModifier) {
-        Text("Username", modifier = paddingModifier)
-        TextField(
-            value = text,
-            onValueChange = {
-                text = it
-            },
-            label = { Text(text = "Choose a Username") },
-        )
+        Column {
+
+            Text("Username", modifier = paddingModifier)
+            TextField(
+                modifier = paddingModifier,
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                label = { Text(text = "Choose a Username") },
+            )
+            Row(modifier = paddingModifier.align(alignment = Alignment.End)) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    content = { Text(text = "Access") }
+                )
+            }
+        }
     }
 }
 
