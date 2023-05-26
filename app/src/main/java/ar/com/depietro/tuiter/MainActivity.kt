e9112bc337cb76c0cf3fefcdf35a8d572eebff3d
Maximiliano De Pietro <elwindwalker@gmail.com>
2023-05-26 19:30:44 -0300

    chore: :wrench: add hilt and placeholders

--- app/src/main/java/ar/com/depietro/tuiter/MainActivity.kt
+++ app/src/main/java/ar/com/depietro/tuiter/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ar.com.depietro.tuiter.ui.theme.TuiterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
